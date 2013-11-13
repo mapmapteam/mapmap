34e8a91d4f7a9bdfbabace1c1c2d578a2498cd9f
Tats <info@sofianaudry.com>
2013-11-13 06:35:05 +0100

    Added missing glClearColor(0,0,0,0);

--- prototypes/wx-01-simple/main.cpp
+++ prototypes/wx-01-simple/main.cpp
@@ -202,6 +202,7 @@ void wxGLCanvasSubClass::Render()
     }
     ratio = (float) GetSize().x / (float) GetSize().y;
 
+    glClearColor(0, 0, 0, 0);
     glClear(GL_COLOR_BUFFER_BIT);
 //    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glViewport(0, 0, (GLint) GetSize().x, (GLint) GetSize().y);
