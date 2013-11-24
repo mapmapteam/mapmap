724c1fc67d01cf804004af03e417caab3fa7cff7
Alexandre Quessy <alexandre@quessy.net>
2013-11-23 19:50:20 -0500

    add shm proto

---
+++ prototypes/shm/write.sh
@@ -0,0 +1,5 @@
+gst-launch -v videotestsrc ! \
+   'video/x-raw-yuv, width=640, height=480, framerate=30/1, format=(fourcc)I420' ! \
+   shmsink socket-path=/tmp/test shm-size=10000000 wait-for-connection=0
+
+
