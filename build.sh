3addaf07287499eadb28eb7e03af3b9220578e1d
Tats <info@sofianaudry.com>
2013-11-24 10:39:16 -0500

    Changed build to work with Qt4

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# libwxgtk2.8-dev
-g++ main.cpp Common.cpp DestinationGLCanvas.cpp SourceGLCanvas.cpp MapperGLCanvas.cpp -o run `wx-config --libs --cxxflags --gl-libs` -lglut -lGL -lSOIL -I/usr/include
+# libwxgtk2.8-dev qt4-default qt4-qmake
+qmake-qt4
+make
