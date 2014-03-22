c54432694eb7de534edeeeff6c62f2321a26c83b
Alexandre Quessy <alexandre@quessy.net>
2014-03-22 15:53:37 -0400

    fix build.sh for some proto

--- prototypes/wx/wx-03-polyline/build.sh
+++ prototypes/wx/wx-03-polyline/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -o verbose
+
 # libwxgtk2.8-dev
-g++ main.cpp -o run `wx-config --libs --cxxflags --gl-libs`
+g++ main.cpp -o run `wx-config --libs --cxxflags --gl-libs`  -lGL -lGLU -lglut
+
+# g++ main.cpp -o run -I/usr/lib/x86_64-linux-gnu/wx/include/gtk2-unicode-release-2.8 -I/usr/include/wx-2.8 -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -pthread -lwx_gtk2u_gl-2.8 -L/usr/lib/x86_64-linux-gnu -pthread -Wl,-Bsymbolic-functions -Wl,-z,relro  -L/usr/lib/x86_64-linux-gnu   -lwx_gtk2u_richtext-2.8 -lwx_gtk2u_aui-2.8 -lwx_gtk2u_xrc-2.8 -lwx_gtk2u_qa-2.8 -lwx_gtk2u_html-2.8 -lwx_gtk2u_adv-2.8 -lwx_gtk2u_core-2.8 -lwx_baseu_xml-2.8 -lwx_baseu_net-2.8 -lwx_baseu-2.8 
