12f1c3a66701b2596003a608984d8957a9da9782
Alexandre Quessy <alexandre@quessy.net>
2014-04-26 00:09:30 +0100

    specify gst 0.10 in shell scripts

--- prototypes/shm/read.sh
+++ prototypes/shm/read.sh
@@ -1,4 +1,4 @@
-gst-launch -e shmsrc socket-path=/tmp/test is-live=1 ! \
+gst-launch-0.10 -e shmsrc socket-path=/tmp/test is-live=1 ! \
     video/x-raw-yuv,format=\(fourcc\)I420,framerate=30/1,width=640,height=480 ! \
     xvimagesink 
 
