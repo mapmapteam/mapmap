97d56c472a8b0c81d6c8b5ae27c339ec75494c62
Alexandre Quessy <alexandre@quessy.net>
2015-10-27 10:02:22 -0400

    Make sure the splash screen image is packaged

--- main.cpp
+++ main.cpp
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
     qFatal("This system has no OpenGL support.");
 
   // Create splash screen.
-  QPixmap pixmap("splash.png");
+  QPixmap pixmap(":/mapmap-splash");
   QSplashScreen splash(pixmap);
 
   // Show splash.
