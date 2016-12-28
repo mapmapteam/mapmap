a445968d2ea6866b104070b0125c8b8bdbd64f7d
Alexandre Quessy <alexandre@quessy.net>
2016-12-28 13:52:41 -0500

    Fix #303: Update list of contributors by hand

--- AboutDialog.h
+++ AboutDialog.h
@@ -43,7 +43,7 @@ public slots:
 private:
   void createAboutTab();
   void createChangelogTab();
-  void createLibrairiesTab();
+  void createLibrariesTab();
   void createContributorsTab();
   void createLicenseTab();
 
